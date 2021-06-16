1f85b85fd690ffd4b18309ef3b52f73e9b925ba7
Ax51 <80718359+Ax51@users.noreply.github.com>
2021-06-16 14:36:10 +0300

    initial functionality for redux training added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <>
-        1
-    </>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0
+        }
+    }
+
+    add = () => {
+        this.setState({count: this.state.count + 1})
+    }
+    remove = () => {
+        this.setState({count: this.state.count - 1})
+    }
+    render() {
+        return (
+            <div className="app">
+                <div className="app--counter">
+                    {this.state.count}
+                </div>
+                <div className="app--control-panel">
+                    <button
+                        className="app--control-panel--btn"
+                        onClick={this.add}>Вычесть</button>
+                    <button
+                        className="app--control-panel--btn"
+                        onClick={this.remove}>Добавить</button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
